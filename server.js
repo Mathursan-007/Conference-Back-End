f14a35c9544753c024f079c5b023dd950ed172ad
Mathursan-007 <63546506+Mathursan-007@users.noreply.github.com>
2021-07-02 16:24:04 +0530

    server commit

--- server.js
+++ server.js
@@ -36,11 +36,25 @@ app.use("/reviewer",ReviewerRoutes);
 app.use("/user",UserRoutes);
 
 
-app.listen(5000, err => {
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(Express.static(path.join(__dirname, 'conference-front-end/build')));
+// Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'conference-front-end/build', 'index.html'));
+    });
+}
+
+
+
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, err => {
     if (err) {
         console.log(err);
         return;
     }
-    console.log('Backend Service is running on port 5000...');
+    console.log('Backend Service is running on port '+port);
 });
 
